f2495f1f649e7bfd465351c4f0b71f985ee8e912
lvlingling <895269560@qq.com>
2018-07-01 11:59:17 +0800

    all

---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="translateForm">
+        <form v-on:submit="formSubmit">
+            <input type="text" placeholder="请输入要翻译的内容" v-model="textToTranslate">
+            <select name="" id="" v-model="language">
+                <option value="en">English</option>
+                <option value="ru">Russian</option>
+                <option value="ko">Korean</option>
+                <option value="ja">Janpanese</option>
+
+
+            </select>
+            <input type="submit" value="翻译">
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'translateForm',
+        data:function(){
+            return {
+                textToTranslate:'',
+                language:""
+            }
+        },
+        methods:{
+            formSubmit:function(e){
+//                alert(this.textToTranslate);
+                this.$emit('formSubmit',this.textToTranslate,this.language);//名字随意
+                e.preventDefault();
+
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
